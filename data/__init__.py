e939d8f79fe3eb6f3dd32e967a34530d00f45c8e
salmank255 <salmank255@gmail.com>
2021-12-16 17:08:20 +0000

    added evaluation for Road_R

--- data/__init__.py
+++ data/__init__.py
@@ -1,2 +1,4 @@
-from .inference_dataset import VideoDataset, custum_collate, get_gt_video_list, get_video_tubes
+# from .inference_dataset import VideoDataset, custum_collate, get_gt_video_list, get_video_tubes
+# from .inference_dataset import VideoDataset, custum_collate, get_gt_video_list, get_video_tubes
+from .dataset_R import VideoDataset, custum_collate, get_gt_video_list, get_video_tubes
 
